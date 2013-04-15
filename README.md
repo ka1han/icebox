742c65287ff97ce9166459552c47d960c5a63018
Twozi <a1009310171@126.com>
2013-04-15 16:56:36 +0800

    Revert "icebox-sorry"
    This reverts commit e5abe5f253f86a7773dc66fd5cc85639bf2e38ef.

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 icebox
 ======
 
-Django的写的博客，基于某篇文章的开源博客= =将会集成一些功能。
-目前版本1.0
-icebox-sorry
-
+django de blog.
--- plug/admin.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from django.contrib import admin
-from plug.models import *
-
-admin.site.register(Xss)
